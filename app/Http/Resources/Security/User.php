8000d6b8c9450b1c88ea2f393fe66711f5cb1f5f
carlos roberto <carlosmoraesas@gmail.com>
2021-03-10 01:49:14 -0300

    Ajuste do Resource retornado no endpoint dos dados do usuário: incluídas as roles.

--- app/Http/Resources/Security/User.php
+++ app/Http/Resources/Security/User.php
@@ -14,6 +14,14 @@ class User extends JsonResource
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'email' => $this->email,
+            'roles' => $this->roles,
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at,
+        ];
+        // return parent::toArray($request);
     }
 }
