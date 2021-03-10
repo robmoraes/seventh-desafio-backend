4d78f4abcfc75768eb8440ff2150cb6be2a9e13e
carlos roberto <carlosmoraesas@gmail.com>
2021-03-10 02:32:36 -0300

    Ajuste do Resource para endpoint do Log de Atividades.

--- app/Http/Resources/Activitylog.php
+++ app/Http/Resources/Activitylog.php
@@ -14,6 +14,17 @@ class Activitylog extends JsonResource
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        return [
+            'id' => $this->id,
+            'user_id' => $this->user_id,
+            'user_name' => $this->user->name,
+            'description' => $this->description,
+            'ip' => $this->ip,
+            'useragent' => $this->useragent,
+            'created_at' => date('d/m/Y H:i:s', strtotime($this->created_at)),
+            'updated_at' => date('d/m/Y H:i:s', strtotime($this->updated_at)),
+            'user' => $this->user,
+        ];
+        // return parent::toArray($request);
     }
 }
